6bb56c70df88295cac18d303eb6c52412417e70b
mpslxz <seyedmehran.pesteie@gmail.com>
2017-03-06 16:36:37 -0800

    ENH: Add log-likelihood cost

--- custom_costs.py
+++ custom_costs.py
@@ -9,3 +9,6 @@ def dice(output, label):
     intersection = T.sum(pred * target)
     dice_value = (2. * intersection + smooth) / (T.sum(pred) + T.sum(target) + smooth)
     return dice_value
+
+def log_likelihood(output, label):
+    return T.mean(T.log(output)[T.arange(label.shape[0]), label])
